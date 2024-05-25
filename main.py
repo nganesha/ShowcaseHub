26fac82bd50a04627d5c9527fff3480277e45cbe
Navin Ganeshan <navin.narayana@gmail.com>
2024-05-25 11:23:12 -0600

    Added appliction details

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import streamlit as st
+import pandas as pd
+
+#col0 = st.columns(1)
+
+#with col0:
+#    st.title("""My Introduction""")
 
 col1, col2 = st.columns(2)
-
 with col1:
-    st.title("""My Introduction""")
+    
     st.image("images/photo.png")
 
 with col2:
@@ -17,6 +22,23 @@ with col2:
     '''
     st.info(content)
 
-    content2 = "Below are some of the apps that I have built!"
-    st.info(content2)
+content2 = "Below are some of the apps that I have built!"
+st.info(content2)
 
+col3, col4 = st.columns(2)
+
+df = pd.read_csv('data.csv', sep=';')
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
